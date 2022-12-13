b89fc34679f5883515a850eac53fa37e1093321e
WiktorSmura <wiktorsmura24@gmail.com>
2022-12-13 19:24:24 +0100

    flat mesh

---
+++ terrain-generator/Helpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace terrain_generator
+{
+    public static class Helpers
+    {
+        /*public static byte[] Reverse(this byte[] b)
+        {
+            Array.Reverse(b);
+            return b;
+        }
+
+        public static Int16 ReadInt16BE(this BinaryReader binRdr)
+        {
+            return BitConverter.ToInt16(binRdr.ReadBytes(sizeof(Int16)).Reverse(), 0);
+        }
+
+        public static int getElement(int[] data, int elementsInRow, int[] coords)
+        {
+            return data[(coords[1] * elementsInRow) + coords[1]];
+        }
+
+        public static Tuple<int[], int> getData(string filePath)
+        {
+            Console.WriteLine("{0}", filePath);
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                int fsLength = (int)new FileInfo(filePath).Length;
+                int[] heights = new int[fsLength / 2];
+                int totalRows = (int)Math.Sqrt(fsLength / 2);
+
+                BinaryReader reader = new BinaryReader(fs, Encoding.BigEndianUnicode);
+                for (int i = 0; i < fsLength / 2; i++)
+                {
+                    heights[i] = ReadInt16BE(reader);
+                }
+
+                return Tuple.Create(heights, totalRows);
+            }
+        } */
+    }
+}
---
+++ terrain-generator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace terrain_generator
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //string hgtPath = "N50E017.hgt";
+            //Tuple<int[], int> dataHgt = Helpers.getData(hgtPath);
+            //Console.WriteLine("x:21, y:37, z:{0}", Helpers.getElement(dataHgt.Item1, dataHgt.Item2, new int[] { 21, 37 }));
+
+            // Setup form
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
